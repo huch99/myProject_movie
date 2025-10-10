a3af19282285dff98068f66ccb3d98ac1c805831
huch99 <huichan58@naver.com>
2025-10-11 00:37:08 +0900

    삭제

--- movie-reservation-project/src/pages/BookingCompletePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BookingComplete from '../components/booking/BookingComplete';
-
-const BookingCompletePage: React.FC = () => {
-  return <BookingComplete />;
-};
-
-export default BookingCompletePage;
